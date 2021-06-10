03378c4a195145046e3655a4361087cdb6c62765
Asiphiwe <cphiwicpour@gmail.com>
2021-06-10 16:53:40 +0200

    Updated Branch

--- src/test/java/za/ac/cput/Factory/ClientAccountTest.java
+++ src/test/java/za/ac/cput/Factory/ClientAccountTest.java
@@ -5,19 +5,53 @@ package za.ac.cput.Factory;
     @Student Number: 218336675
     @Date: 04 June 2021
  */
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.ClientAccount;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClientAccountTest
 {
+    ClientAccount account = ClientAccountFactory.createClientAccount("4","0325");
     @Test
     public void createClientAccountTest(){
-        ClientAccount account = ClientAccountFactory.createClientAccount("4","0325");
+
+        System.out.println(account);
+        assertNotNull(account);
+    }
+    @BeforeEach
+
+    @Test
+    void testEquality()
+    {
+
+        assertEquals("0325",account.getAccountNum());
+
+    }
+    @Test
+    void testIdentity()
+    {
+        assertSame("4",account.getNumBorrowed());
+    }
+    @Test
+    @Disabled
+    void  createClientAccountDisable()
+    {
         System.out.println(account);
         assertNotNull(account);
     }
 
+    @Test
+    @Timeout(10)
+    void timeOut() throws InterruptedException
+    {
+        Thread.sleep(100);
+        System.out.println("Time up for test");
+    }
 
 }
