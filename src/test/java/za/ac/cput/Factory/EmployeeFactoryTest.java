a4eff0387bfa10287ca57ef8d98a3d9a3aec3b0e
TyronneLloyd <215141210@mycput.ac.za>
2021-06-10 16:18:35 +0200

    With updated files

--- src/test/java/za/ac/cput/Factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/EmployeeFactoryTest.java
@@ -8,14 +8,17 @@ package za.ac.cput.Factory;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.Employee;
 
 class EmployeeFactoryTest {
         @Test
+        @Timeout(1)
         public void createEmployee(){
             Employee employee = EmployeeFactory.createEmployee("Ty","Lloyd", "admin","215141210","cput");
             assertNotNull(employee);
             System.out.println(employee);
     }
+
 
 }//class
