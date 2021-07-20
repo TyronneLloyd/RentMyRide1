349f434ac7fbeb69391f64d9bd609f914085ead4
TyronneLloyd <215141210@mycput.ac.za>
2021-07-20 21:34:06 +0200

    partially completed Testing, getAll() is not working.

--- src/main/java/za/ac/cput/repository/employee/IEmployeeRepository.java
+++ src/main/java/za/ac/cput/repository/employee/IEmployeeRepository.java
@@ -1,5 +1,10 @@
 package za.ac.cput.repository.employee;
-
+/*
+    @Description:Repository ->
+    @Author: Tyronne Lloyd Hendricks
+    @Student Number: 215141210
+    @Date: 19 July 2021
+*/
 import za.ac.cput.entity.Employee;
 import za.ac.cput.repository.IRepository;
 import java.util.Set;
