4c37c5cbf3331db39e348f19f4647227eac3892c
elrichherman <juniorherman1@gmail.com>
2021-07-22 22:10:42 +0200

    Update

---
+++ src/main/java/za/ac/cput/repository/carlot/impl/CarLotRepository.java
@@ -0,0 +1,79 @@
+package za.ac.cput.repository.carlot.impl;
+
+/*
+   @Description:CarLot.java
+   @Author: Elrich Herman
+   @Student Number: 218150040
+   @Date: 22 July 2021
+ */
+
+import za.ac.cput.entity.CarLot;
+import za.ac.cput.repository.carlot.ICarLotRepository;
+import java.util.*;
+
+public class CarLotRepository implements ICarLotRepository {
+
+    private static CarLotRepository repository = null;
+    private Set<CarLot> carLots = null;
+
+    private CarLotRepository()
+    {
+        carLots = new HashSet<CarLot>();
+    }
+
+    public static CarLotRepository getRepository()
+    {
+        if(repository == null){
+            repository = new CarLotRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public CarLot create(CarLot carLot)
+    {
+        boolean success = carLots.add(carLot);
+        if(!success)
+            return null;
+        return carLot;
+    }
+
+    @Override
+    public CarLot read(String carLotID)
+    {
+        for (CarLot c: carLots)
+            if(c.getNumberPlate().equalsIgnoreCase(carLotID)){
+                return c;
+            }
+        return null;
+    }
+
+    @Override
+    public CarLot update(CarLot carLot)
+    {
+        CarLot oldCarLot = read(carLot.getNumberPlate());
+        if(oldCarLot != null){
+            carLots.remove(oldCarLot);
+            carLots.add(carLot);
+            return carLot;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String carLotID)
+    {
+        CarLot carLotRemoved = read(carLotID);
+        if(carLotRemoved == null)
+            return false;
+        carLots.remove(carLotRemoved);
+        return true ;
+    }
+
+    @Override
+    public Set<CarLot> getAll()
+    {
+        return carLots;
+    }
+}
+
---
+++ src/test/java/za/ac/cput/repository/carlot/impl/CarLotRepositoryTest.java
@@ -0,0 +1,64 @@
+package za.ac.cput.repository.carlot.impl;
+
+/*
+   @Description:CarLot.java
+   @Author: Elrich Herman
+   @Student Number: 218150040
+   @Date: 22 July 2021
+ */
+
+import org.junit.jupiter.api.*;
+import za.ac.cput.entity.CarLot;
+import za.ac.cput.factory.CarLotFactory;
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+class CarLotRepositoryTest
+{
+    private static CarLotRepository repository = CarLotRepository.getRepository();
+    private static CarLot carLot = CarLotFactory.createCarLot("84D546Y4","54HH");
+
+    @Test
+    @Order(1)
+    void create()
+    {
+        CarLot create = repository.create(carLot);
+        assertEquals(create.getNumberPlate(),carLot.getNumberPlate());
+        System.out.println("Created: " + create);
+    }
+
+    @Test
+    @Order(2)
+    void read()
+    {
+        CarLot read = repository.read(carLot.getNumberPlate());
+        assertNotNull(read);
+        System.out.println("Read: " + read);
+    }
+
+    @Test
+    @Order(3)
+    void update()
+    {
+        CarLot updated = new CarLot.Builder().copy(carLot).setParkingSpace("54HH").build();
+        assertNotNull(repository.update(updated));
+        System.out.println("Updated: " + updated);
+    }
+    @Test
+    @Order(4)
+    void getAll()
+    {
+        System.out.println("Display All: ");
+        System.out.println(repository.getAll());
+    }
+
+    @Test
+    @Order(5)
+    void delete()
+    {
+        boolean success = repository.delete(carLot.getNumberPlate());
+        assertTrue(success);
+        System.out.println("Deleted: " + success);
+    }
+
+}
