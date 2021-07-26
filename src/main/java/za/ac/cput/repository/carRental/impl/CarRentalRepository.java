6a08d891a84dec2fb58f6974c9b3ff40e028282b
215138848 <215138848@mycput.ac.za>
2021-07-26 16:08:44 +0200

    repository's

---
+++ src/main/java/za/ac/cput/repository/carRental/impl/CarRentalRepository.java
@@ -0,0 +1,10 @@
+package za.ac.cput.repository.carRental.impl;
+/*
+    @Description: CarRental.java -> Car Rental class facilitates the renting of cars and records them
+    @Author: Grant Hendricks
+    @Student Number: 215138848
+    @Date: 23 July 2021
+  */
+
+public class CarRentalRepository {
+}
