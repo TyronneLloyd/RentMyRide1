c104a1e63c7bb9c7193c2d417b04e4e0933aaaf9
215138848 <215138848@mycput.ac.za>
2021-07-26 20:35:07 +0200

    all test working files

--- src/main/java/za/ac/cput/repository/carRental/impl/CarRentalRepository.java
+++ src/main/java/za/ac/cput/repository/carRental/impl/CarRentalRepository.java
@@ -1,6 +1,5 @@
 package za.ac.cput.repository.carRental.impl;
 /*
-    @Description: CarRental.java -> Car Rental class facilitates the renting of cars and records them
     @Author: Grant Hendricks
     @Student Number: 215138848
     @Date: 23 July 2021
@@ -42,10 +41,10 @@ public class CarRentalRepository implements ICarRentalRepository {
     @Override
     public CarRental read(String rentalID) {
         for (CarRental cr: carRentalDB)
-            if (cr.getRentalID().equalsIgnoreCase(rentalID)){
+            if (cr.getRentalID().equals(rentalID)){
                 return cr;
             }
-        return null;
+            return null;
     }
 
     @Override
--- src/test/java/za/ac/cput/repository/carRental/impl/CarRentalRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/carRental/impl/CarRentalRepositoryTest.java
@@ -1,6 +1,5 @@
 package za.ac.cput.repository.carRental.impl;
 /*
-    @Description: CarRental.java -> Car Rental class facilitates the renting of cars and records them
     @Author: Grant Hendricks
     @Student Number: 215138848
     @Date: 25 July 2021
@@ -19,35 +18,35 @@ class CarRentalRepositoryTest {
     private static CarRental carRental = CarRentalFactory.createCarRental("client57","2cool4u","1000006","25-07-2021","28-08-2021");
 
     @Test
-    void create() {
+    void a_create() {
         CarRental created = repository.create(carRental);
         assertEquals(created.getRentalID() ,carRental.getRentalID() );
         System.out.println("Create Test: " + created);
     }
 
     @Test
-    void read() {
-        CarRental read = repository.read(carRental.getNumberPlate());
+    void b_read() {
+        CarRental read = repository.read(carRental.getRentalID());
         assertNotNull(read);
         System.out.println("Read Test: " + read);
     }
 
     @Test
-    void update() {
-        CarRental updated = new CarRental.Builder().copy(carRental).setRentalEndDate("11-09-2021").build();
-        assertNotNull(repository.update(updated));
-        System.out.println("Update Test: " + updated);
+    void c_update() {
+        CarRental myUpdated = new CarRental.Builder().copy(carRental).setRentalEndDate("11-09-2021").setClientID("client01").setNumberPlate("2cool4u").setEmployeeNumber("1000001").build();
+        assertNotNull(repository.update(myUpdated));
+        System.out.println("Update Test: " + myUpdated);
     }
 
     @Test
-    void delete() {
+    void e_delete() {
         boolean success = repository.delete(carRental.getRentalID());
         assertTrue(success);
         System.out.println("Delete Test: " + success);
     }
 
     @Test
-    void getAll() {
+    void d_getAll() {
         System.out.println("Get All Test: \n" + repository.getAll());
     }
 }
