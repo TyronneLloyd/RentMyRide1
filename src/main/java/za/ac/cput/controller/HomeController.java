fb373d2ff95d1a7096dc484750c61f511030d7cd
Acphiwer <75356777+Acphiwer@users.noreply.github.com>
2021-08-25 14:54:34 +0200

    home controller

--- src/main/java/za/ac/cput/controller/HomeController.java
+++ src/main/java/za/ac/cput/controller/HomeController.java
@@ -1,30 +1,18 @@
 package za.ac.cput.controller;
 
-<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.RequestMapping;
-=======
-<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-=======
 import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> main
->>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 
     @GetMapping("/")
     public String index() {
         return "Welcome to RentMyRide online shop";
     }
 
-=======
-    
->>>>>>> main
->>>>>>> Stashed changes
+
 }
